2719b08aed4caf5f2854f53816ac92b9c362341f
ossicl <oscroyale@gmail.com>
2024-11-20 14:37:27 +0100

    2.4

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock =  pygame.time.Clock()
+    dt = 0
+
+    # Game loop
     while True:
 
         for event in pygame.event.get():
@@ -18,6 +22,8 @@ def main():
         screen.fill(color=000000)
         pygame.display.flip()
 
+        clock.tick(60)
+        dt = clock.tick()/1000
 
 
 
